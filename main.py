f8ed825d00e17dc88184c4fd1a9670527ea28544
Rayyaan-Mustafa <Rayyaan-Mustafa@users.noreply.github.com>
2022-12-08 13:50:42 -0800

    everything should work...

--- main.py
+++ main.py
@@ -2,100 +2,120 @@ import numpy as np
 import math
 import time
 import copy
+#logger
+import logging
 
 def automate_tests():
     results = []
-    files = ["CS170_Large_Data__21.txt","CS170_Large_Data__99.txt","CS170_Small_Data__88.txt","CS170_Large_Data__6.txt","CS170_Small_Data__6.txt","CS170_Small_Data__96.txt","CS170_Large_Data__96.txt","CS170_Small_Data__64.txt"]
-    start = time.time()
-    end = 0
-    for f in files:
-        try:
-            data= np.loadtxt(f)
-            #runs forward selection on each file
-            OVERALL_best_accuracy = 0
-            OVERALL_best_set = []
-            current_set_of_features = [] #initialize empty set
-            for i in range(1, len(data[0])):
-                # print("On the " + str(i) + "th level of the search tree")
-                feature_to_add_at_this_level = None
-                best_accuracy_so_far = 0
-                for k in range(1,len(data[0])):
-                    if k not in current_set_of_features:#only consider adding, if not already added
-                        # print("--Considering adding the " + str(k) + " feature")
-                        accuracy = leave_one_out_cross_validation(data, current_set_of_features, k)
-                        if accuracy > best_accuracy_so_far:
-                            best_accuracy_so_far = accuracy
-                            feature_to_add_at_this_level = k
-                if best_accuracy_so_far > OVERALL_best_accuracy:
-                    OVERALL_best_accuracy = best_accuracy_so_far
-                    current_set_of_features.append(feature_to_add_at_this_level)
-                    OVERALL_best_set = copy.deepcopy(current_set_of_features)
-                else:
-                    current_set_of_features.append(feature_to_add_at_this_level)
-                # print("On level " + str(i) + " I added feature " + str(feature_to_add_at_this_level) + " to the current set")
-                print("Feature set " + str(current_set_of_features) + " was best, accuracy is " + str(best_accuracy_so_far*100) + "%")
-            print("Finished search!! The best feature subset is " + str(OVERALL_best_set) + ", which has an accuracy of " + str(OVERALL_best_accuracy*100) + "%")
-            end=time.time()
-            results.append((f,OVERALL_best_accuracy,OVERALL_best_set,end-start))
-        except Exception as e:
-            results.append((f,e))
-    print(results)
+    files = ["CS170_Small_Data__64.txt","CS170_Large_Data__99.txt"]
+
+    try:
+        logging.info("Starting tests")
+        logging.info("Testing forward selection")
+        for f in files:
+            data = np.loadtxt(f) #read the file into an numpy array
+            start = time.time()
+            logging.info("Testing file: " + f)
+            forward_selection(data)
+            end = time.time()
+            logging.info("Time taken: " + str(round(end-start,2)) + " seconds")
+        logging.info("Testing backward elimination")
+        for f in files:
+            data = np.loadtxt(f) #read the file into an numpy array
+            start = time.time()
+            logging.info("Testing file: " + f)
+            backward_elimination(data)
+            end = time.time()
+            logging.info("Time taken: " + str(round(end-start,2)) + " seconds")
+    except Exception as e:
+        logging.error("Error in testing: " + str(e))
+    
 def main():
     print('Welcome to Rayyaan\'s Feature Selection Algorithm.')
     fname = input('Type in the name of the file to test : ')
     algoChoice = input('Type in the number of the algorithm you want to run.\n    1) Forward Selection\n    2) Backward Elimination\n')
     data = np.loadtxt(fname) #read the file into an numpy array
     print("This dataset has " + str(len(data[0]) - 1) + " features (not including the class attribute), with " + str(len(data)) + " instances.")
-    print("Running nearest neighbor with all " + str(len(data[0]) - 1) + " features, using \"leaving-one-out\" evaluation, I get an accuracy of " + str(leave_one_out_cross_validation(data, set(range(1,len(data[0]))), 0)*100) + "%")
+    print("Running nearest neighbor with all " + str(len(data[0]) - 1) + " features, using \"leaving-one-out\" evaluation, I get an accuracy of " + str(leave_one_out_cross_validation(data, set(range(1,len(data[0]))), 0,0)*100) + "%")
     print("Beginning search")
     start = time.time()
-    end = 0
     #forward selection
     if algoChoice == '1':
-        OVERALL_best_accuracy = 0
-        OVERALL_best_set = []
-        current_set_of_features = [] #initialize empty set
-        for i in range(1, len(data[0])):
-            # print("On the " + str(i) + "th level of the search tree")
-            feature_to_add_at_this_level = None
-            best_accuracy_so_far = 0
-            for k in range(1,len(data[0])):
-                if k not in current_set_of_features:#only consider adding, if not already added
-                    # print("--Considering adding the " + str(k) + " feature")
-                    accuracy = leave_one_out_cross_validation(data, current_set_of_features, k)
-                    if accuracy > best_accuracy_so_far:
-                        best_accuracy_so_far = accuracy
-                        feature_to_add_at_this_level = k
-            if best_accuracy_so_far > OVERALL_best_accuracy:
-                OVERALL_best_accuracy = best_accuracy_so_far
-                current_set_of_features.append(feature_to_add_at_this_level)
-                OVERALL_best_set = copy.deepcopy(current_set_of_features)
-            else:
-                current_set_of_features.append(feature_to_add_at_this_level)
-            # print("On level " + str(i) + " I added feature " + str(feature_to_add_at_this_level) + " to the current set")
-            print("Feature set " + str(current_set_of_features) + " was best, accuracy is " + str(best_accuracy_so_far*100) + "%")
-        print("Finished search!! The best feature subset is " + str(OVERALL_best_set) + ", which has an accuracy of " + str(OVERALL_best_accuracy*100) + "%")
-        end = time.time()
-    #backward elimination
+        forward_selection(data)
     elif algoChoice == '2':
-        #insert backward elimination code here
-        end = time.time()
+        backward_elimination(data)
     else:
         print("Incorrect algoritm choice. Please try again.")
         return
+    end = time.time()
+    print("Search took " + str(round(end-start,2)) + " seconds.")#spurious precision!!
     
 
+def forward_selection(data):
+    OVERALL_best_accuracy = 0
+    OVERALL_best_set = []
+    current_set_of_features = [] #initialize empty set
+    for i in range(1, len(data[0])):
+        # print("On the " + str(i) + "th level of the search tree")
+        feature_to_add_at_this_level = None
+        best_accuracy_so_far = 0
+        for k in range(1,len(data[0])):
+            if k not in current_set_of_features:#only consider adding, if not already added
+                # print("--Considering adding the " + str(k) + " feature")
+                accuracy = leave_one_out_cross_validation(data, current_set_of_features, k,1)
+                if accuracy > best_accuracy_so_far:
+                    best_accuracy_so_far = accuracy
+                    feature_to_add_at_this_level = k
+        if best_accuracy_so_far > OVERALL_best_accuracy:
+            OVERALL_best_accuracy = best_accuracy_so_far
+            current_set_of_features.append(feature_to_add_at_this_level)
+            OVERALL_best_set = copy.deepcopy(current_set_of_features)
+        else:
+            current_set_of_features.append(feature_to_add_at_this_level)
+        # print("On level " + str(i) + " I added feature " + str(feature_to_add_at_this_level) + " to the current set")
+        print("Feature set " + str(current_set_of_features) + " was best, accuracy is " + str(best_accuracy_so_far*100) + "%")
+    print("Finished search!! The best feature subset is " + str(OVERALL_best_set) + ", which has an accuracy of " + str(OVERALL_best_accuracy*100) + "%")
+    logging.info("Finished search!! The best feature subset is " + str(OVERALL_best_set) + ", which has an accuracy of " + str(OVERALL_best_accuracy*100) + "%")
+    
+def backward_elimination(data):
+    OVERALL_best_accuracy = 0
+    OVERALL_best_set = []
+    current_set_of_features = set(range(1,len(data[0])))#initialize set to all features
+    for i in range(1, len(data[0])):
+        feature_to_remove_at_this_level = None
+        best_accuracy_so_far = 0
+        for k in range(1,len(data[0])):
+            if k in current_set_of_features:#only consider removing, if not already removed
+                accuracy = leave_one_out_cross_validation(data, current_set_of_features, k,2)
+                if accuracy > best_accuracy_so_far:
+                    best_accuracy_so_far = accuracy
+                    feature_to_remove_at_this_level = k
+        if best_accuracy_so_far > OVERALL_best_accuracy:
+            OVERALL_best_accuracy = best_accuracy_so_far
+            current_set_of_features.remove(feature_to_remove_at_this_level)
+            OVERALL_best_set = copy.deepcopy(current_set_of_features)
+        else:
+            current_set_of_features.remove(feature_to_remove_at_this_level)
+        print("Feature set " + str(current_set_of_features) + " was best, accuracy is " + str(best_accuracy_so_far*100) + "%")
+    print("Finished search!! The best feature subset is " + str(OVERALL_best_set) + ", which has an accuracy of " + str(OVERALL_best_accuracy*100) + "%")
+    logging.info("Finished search!! The best feature subset is " + str(OVERALL_best_set) + ", which has an accuracy of " + str(OVERALL_best_accuracy*100) + "%")
 
-def leave_one_out_cross_validation(data, current_set_of_features, feature_to_add):
+    
+
+def leave_one_out_cross_validation(data, current_set_of_features, feature_to_add_or_remove, algo):
     # return np.random.rand()
     current_set_of_features_copy = copy.deepcopy(current_set_of_features)
-    if feature_to_add != 0:
-        current_set_of_features_copy.append(feature_to_add)
+    if algo == 1:
+        if feature_to_add_or_remove != 0:
+            current_set_of_features_copy.append(feature_to_add_or_remove)
+    elif algo == 2:
+        if feature_to_add_or_remove != 0:
+            current_set_of_features_copy.remove(feature_to_add_or_remove)
     number_correctly_classified = 0
     data_copy = copy.deepcopy(data)
     unused_features = set(range(1,len(data[0]))) - set(current_set_of_features_copy)
     # print("current set of features: " + str(current_set_of_features_copy))  
-    # print("feature_to_add" + str(feature_to_add))
+    # print("feature_to_add_or_remove" + str(feature_to_add_or_remove))
     for i in range(0, len(data_copy)):
         for f in unused_features:
                 data_copy[i][f] = 0 #(Slide 65) set the feature values of the unused features to 0
@@ -123,5 +143,7 @@ def leave_one_out_cross_validation(data, current_set_of_features, feature_to_add
     return accuracy
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='app.log' + str(time.time()), filemode='w', format='%(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+
     main()
     # automate_tests()
