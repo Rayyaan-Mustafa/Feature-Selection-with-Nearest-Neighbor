87372a77bd60c8d994b2b3815c1ea0f764fd6fd5
Rayyaan-Mustafa <Rayyaan-Mustafa@users.noreply.github.com>
2022-12-08 17:07:13 -0800

    final?

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import numpy as np
 import math
 import time
 import copy
-#logger
 import logging
 
+#tests to get the numbers for multiple files at once
 def automate_tests():
     files = ["CS170_Small_Data__64.txt","CS170_Large_Data__99.txt"]
-
     try:
         logging.info("Starting tests")
         logging.info("Testing forward selection")
@@ -35,7 +34,7 @@ def main():
     algoChoice = input('Type in the number of the algorithm you want to run.\n    1) Forward Selection\n    2) Backward Elimination\n')
     data = np.loadtxt(fname) #read the file into an numpy array
     print("This dataset has " + str(len(data[0]) - 1) + " features (not including the class attribute), with " + str(len(data)) + " instances.")
-    print("Running nearest neighbor with all " + str(len(data[0]) - 1) + " features, using \"leaving-one-out\" evaluation, I get an accuracy of " + str(leave_one_out_cross_validation(data, set(range(1,len(data[0]))), 0,0)*100) + "%")
+    print("Running nearest neighbor with all " + str(len(data[0]) - 1) + " features, using \"leaving-one-out\" evaluation, I get an accuracy of " + str(round(leave_one_out_cross_validation(data, set(range(1,len(data[0]))), 0,0)*100,2)) + "%")
     print("Beginning search")
     start = time.time()
     #forward selection
@@ -47,25 +46,25 @@ def main():
         print("Incorrect algoritm choice. Please try again.")
         return
     end = time.time()
-    print("Search took " + str(round(end-start,2)) + " seconds.")#spurious precision!!
+    print("Search took " + str(round(end-start,1)) + " seconds.")#spurious precision!!
     
 
 def forward_selection(data):
     OVERALL_best_accuracy = 0
     OVERALL_best_set = []
     current_set_of_features = [] #initialize empty set
-    for i in range(1, len(data[0])):
+    for i in range(1, len(data[0])):#iterate through all features
         # print("On the " + str(i) + "th level of the search tree")
         feature_to_add_at_this_level = None
         best_accuracy_so_far = 0
-        for k in range(1,len(data[0])):
+        for k in range(1,len(data[0])):#iterate through all features
             if k not in current_set_of_features:#only consider adding, if not already added
                 # print("--Considering adding the " + str(k) + " feature")
-                accuracy = leave_one_out_cross_validation(data, current_set_of_features, k,1)
-                if accuracy > best_accuracy_so_far:
+                accuracy = leave_one_out_cross_validation(data, current_set_of_features, k,1)#calculate accuracy when you add feature k
+                if accuracy > best_accuracy_so_far:#if accuracy is better than best so far, update best accuracy and feature to add
                     best_accuracy_so_far = accuracy
                     feature_to_add_at_this_level = k
-        if best_accuracy_so_far > OVERALL_best_accuracy:
+        if best_accuracy_so_far > OVERALL_best_accuracy:#if best_accuracy_so_far is better than OVERALL_best_accuracy, update OVERALL_best_accuracy and OVERALL_best_set
             OVERALL_best_accuracy = best_accuracy_so_far
             current_set_of_features.append(feature_to_add_at_this_level)
             OVERALL_best_set = copy.deepcopy(current_set_of_features)
@@ -85,11 +84,11 @@ def backward_elimination(data):
         best_accuracy_so_far = 0
         for k in range(1,len(data[0])):
             if k in current_set_of_features:#only consider removing, if not already removed
-                accuracy = leave_one_out_cross_validation(data, current_set_of_features, k,2)
-                if accuracy > best_accuracy_so_far:
+                accuracy = leave_one_out_cross_validation(data, current_set_of_features, k,2)#calculate accuracy when you remove feature k
+                if accuracy > best_accuracy_so_far:#if accuracy is better than best so far, update best accuracy and feature to remove
                     best_accuracy_so_far = accuracy
                     feature_to_remove_at_this_level = k
-        if best_accuracy_so_far > OVERALL_best_accuracy:
+        if best_accuracy_so_far > OVERALL_best_accuracy:#if best_accuracy_so_far is better than OVERALL_best_accuracy, update OVERALL_best_accuracy and OVERALL_best_set
             OVERALL_best_accuracy = best_accuracy_so_far
             current_set_of_features.remove(feature_to_remove_at_this_level)
             OVERALL_best_set = copy.deepcopy(current_set_of_features)
@@ -103,15 +102,15 @@ def backward_elimination(data):
 
 def leave_one_out_cross_validation(data, current_set_of_features, feature_to_add_or_remove, algo):
     # return np.random.rand()
-    current_set_of_features_copy = copy.deepcopy(current_set_of_features)
-    if algo == 1:
+    current_set_of_features_copy = copy.deepcopy(current_set_of_features)#make a copy of the current set of features to avoid modifying the original
+    if algo == 1:#if we are adding a feature (forward selection)
         if feature_to_add_or_remove != 0:
             current_set_of_features_copy.append(feature_to_add_or_remove)
-    elif algo == 2:
+    elif algo == 2:#if we are removing a feature (backward elimination)
         if feature_to_add_or_remove != 0:
             current_set_of_features_copy.remove(feature_to_add_or_remove)
     number_correctly_classified = 0
-    data_copy = copy.deepcopy(data)
+    data_copy = copy.deepcopy(data)#make a copy of the data to avoid modifying the original
     unused_features = set(range(1,len(data[0]))) - set(current_set_of_features_copy)
     # print("current set of features: " + str(current_set_of_features_copy))  
     # print("feature_to_add_or_remove" + str(feature_to_add_or_remove))
@@ -127,18 +126,18 @@ def leave_one_out_cross_validation(data, current_set_of_features, feature_to_add
 
         for k in range(0,len(data_copy)):#for each row 'k' in the data_copy
             if k != i:
-                distance = math.sqrt(sum((object_to_classify - data_copy[k][1:])**2))
+                distance = math.sqrt(sum((object_to_classify - data_copy[k][1:])**2))#calculating distance between object_to_classify and row 'k'
                 if distance < nearest_neighbor_distance:
                     nearest_neighbor_distance = distance
                     nearest_neighbor_location = k
                     nearest_neighbor_label = data_copy[nearest_neighbor_location][0]
         # print("Object " + str(i) + " is class " + str(label_object_to_classify))
         # print("It's nearest neighbor is " + str(nearest_neighbor_location) + " which is in class " + str(nearest_neighbor_label))
-        if label_object_to_classify == nearest_neighbor_label:
+        if label_object_to_classify == nearest_neighbor_label:#
             number_correctly_classified += 1
     accuracy = number_correctly_classified / len(data_copy)
     # print("Accuracy is " + str(accuracy))
-    print("\tUsing feature(s) " + str(current_set_of_features_copy) + " accuracy is " + str(accuracy*100) + "%")
+    print("\tUsing feature(s) " + str(current_set_of_features_copy) + " accuracy is " + str(round(accuracy*100,2)) + "%")
     return accuracy
 
 if __name__ == '__main__':
